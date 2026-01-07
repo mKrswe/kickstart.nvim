c23396e05f0657f87f4f01c6a956700b40d4a06b
Marco K <kursawemarco@googlemail.com>
2026-01-07 07:33:33 +0100

    Theme

--- init.lua
+++ init.lua
@@ -850,20 +850,24 @@ require('lazy').setup({
     },
   },
 
+  --{
+  --  'catppuccin/nvim',
+  --  name = 'catppuccin',
+  --  priority = 1000,
+  --  vim.cmd.colorscheme 'catppuccin-mocha',
+  --},
   {
-    'cocopon/iceberg.vim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      -- require('gruvbox').setup {
-      --   styles = {
-      --     comments = { italic = false }, -- Disable italics in comments
-      --   },
-      -- }
+      require('catppuccin').setup {
+        flavour = 'auto',
+      }
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'iceberg'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
